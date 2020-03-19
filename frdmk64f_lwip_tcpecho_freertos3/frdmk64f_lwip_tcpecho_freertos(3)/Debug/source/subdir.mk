beab96bef2c01328876dea6f5c647e16c5f849cc
ClyoDC <ie702806@iteso.mx>
2020-03-18 19:57:21 -0600

    p3

---
+++ frdmk64f_lwip_tcpecho_freertos3/frdmk64f_lwip_tcpecho_freertos(3)/Debug/source/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../source/lwip_tcpecho_freertos.c \
+../source/semihost_hardfault.c 
+
+OBJS += \
+./source/lwip_tcpecho_freertos.o \
+./source/semihost_hardfault.o 
+
+C_DEPS += \
+./source/lwip_tcpecho_freertos.d \
+./source/semihost_hardfault.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+source/%.o: ../source/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -std=gnu99 -D__REDLIB__ -DCPU_MK64FN1M0VLL12 -DCPU_MK64FN1M0VLL12_cm4 -DUSE_RTOS=1 -DPRINTF_ADVANCED_ENABLE=1 -DFRDM_K64F -DFREEDOM -DFSL_RTOS_FREE_RTOS -DSDK_DEBUGCONSOLE=0 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I../board -I../source -I../ -I../device -I../CMSIS -I../drivers -I../lwip/contrib/apps/tcpecho -I../lwip/port/arch -I../lwip/src/include/compat/posix/arpa -I../lwip/src/include/compat/posix/net -I../lwip/src/include/compat/posix -I../lwip/src/include/compat/posix/sys -I../lwip/src/include/compat/stdc -I../lwip/src/include/lwip -I../lwip/src/include/lwip/priv -I../lwip/src/include/lwip/prot -I../lwip/src/include/netif -I../lwip/src/include/netif/ppp -I../lwip/src/include/netif/ppp/polarssl -I../lwip/port -I../amazon-freertos/freertos/portable -I../amazon-freertos/include -I../utilities -I../component/lists -I../component/serial_manager -I../component/uart -I../lwip/src -I../lwip/src/include -O0 -fno-common -g3 -Wall -c  -ffunction-sections  -fdata-sections  -ffreestanding  -fno-builtin -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ frdmk64f_lwip_tcpecho_freertos_2/frdmk64f_lwip_tcpecho_freertos(2)/Debug/lwip/contrib/apps/tcpecho/aes.d
@@ -0,0 +1,4 @@
+lwip/contrib/apps/tcpecho/aes.o lwip/contrib/apps/tcpecho/aes.d: \
+ ../lwip/contrib/apps/tcpecho/aes.c ../lwip/contrib/apps/tcpecho/aes.h
+
+../lwip/contrib/apps/tcpecho/aes.h:
